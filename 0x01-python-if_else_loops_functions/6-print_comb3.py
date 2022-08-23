b075b185bd308bfb65d819cb1c9e537f4840dee8
Collins Ogera <ogeracollins@gmail.com>
2022-08-23 19:44:14 +0000

    Possible combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a < b and a != 8:
+            print("{:d}{:d}, ".format(a, b).end='')
+        if a < b and a == 8:
+            print("{:d}{:d}".format(a, b))
