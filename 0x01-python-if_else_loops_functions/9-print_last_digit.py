046694e0834da76a31ae66ac4232ddb58a6a2773
Collins Ogera <ogeracollins@gmail.com>
2022-08-23 20:58:04 +0000

    Function to print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,9 @@ def print_last_number(number):
     if number >= 0:
         rem = number % 10
     else:
-        rem = 10 - (tmp % 10)
+        rem = 10 - (number % 10)
     print("{:d}".format(rem), end='')
     return rem
 
-if __name = '__main__':
-    print_last_gigit(12345)
+
+
