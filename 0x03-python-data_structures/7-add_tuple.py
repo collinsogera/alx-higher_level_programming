f8b02cf443f377e67d7176010fb633bc9882d67c
Collins Ogera <ogeracollins@gmail.com>
2022-08-30 19:38:31 +0000

    Add two tupples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     TupA = tuple_a + (0, 0)
     TupB = tuple_b + (0, 0)
-    new TupA[0], Tup[1] + TupB[1]
+    new = TupA[0], Tup[1] + TupB[1]
     return new
