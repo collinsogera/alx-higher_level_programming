893dcc6c41f9420fcae6ff1ccb2100f0c8b4d26e
Collins Ogera <ogeracollins@gmail.com>
2022-08-30 19:15:00 +0000

    Print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_list_integer(my_list=[]);
     for n in my_list:
-        [rint('{:d}'.format(n))
+        print('{:d}'.format(n))
 
 if __name__ == '__main__':
     my_list = [2, 4, 6, 8]
