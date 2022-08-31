303c89089c3337c4f931484d4f2bf1760e6f0297
Collins Ogera <ogeracollins@gmail.com>
2022-08-31 19:39:10 +0000

    Roman to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,4 +6,27 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman_string[i] == "M":
             number += 10000
-
+            if roman_string[i-1] == "C" and i != 0:
+                number -= 200
+       if roman_string[i] == "D":
+            number += 500
+            if roman_string[i-1] == "C" and i != 0:    
+                number -= 200
+       if roman_string[i] == "C":
+            number += 100
+            if roman_string[i-1] == "X" and i != 0:
+                number += 20
+       if roman_string[i] == "L":
+            number += 50
+            if roman_string[i-1] == "X" and i != 0:
+                number += 20
+       if roman_string[i] == "X":
+            number += 10
+            if roman_string[i-1] == "I" and i != 0:
+                number -= 2
+       if roman_string[i] == "V":
+            number += 5
+            if roman_string[i-1] == "I" and i != 0:
+                number -= 2
+       if roman_string[i] == "I":
+            number += 1
